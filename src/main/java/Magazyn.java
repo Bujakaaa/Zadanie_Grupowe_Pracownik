3a1e5c64c1d606e9d72b25e8db72dc222b9260dc
Paulina Bujała <bujala.paulina@gmail.com>
2019-03-02 15:53:27 +0100

    Dodanie klasy Magazyn

---
+++ src/main/java/Magazyn.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class Magazyn {
+    List <String> listaPracownikow;
+    List <String> listaProduktów;
+
+    public Magazyn(List<String> listaPracownikow, List<String> listaProduktów) {
+        this.listaPracownikow = listaPracownikow;
+        this.listaProduktów = listaProduktów;
+    }
+
+    @Override
+    public String toString() {
+        return "Magazyn{" +
+                "listaPracownikow=" + listaPracownikow +
+                ", listaProduktów=" + listaProduktów +
+                '}';
+    }
+}
