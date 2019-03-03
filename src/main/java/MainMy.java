3047015bdfdb0adc2b747bb7eb256f993e9856d6
Paulina Bujała <bujala.paulina@gmail.com>
2019-03-03 14:52:19 +0100

    Metody osoba 2

--- src/main/java/MainMy.java
+++ src/main/java/MainMy.java
@@ -20,7 +20,7 @@ public class MainMy {
 
         magazyn.usunProdukt("masło");
 
-        System.out.println(magazyn.wypiszProdukt("mąka")); //dziala :)
+        System.out.println(magazyn.wypiszProdukt("masło")); //dziala :)
         System.out.println();
 
         System.out.println(magazyn.wypiszAllProdukty()); //dziala
