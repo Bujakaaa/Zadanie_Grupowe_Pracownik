5fc68545a6f28fc6b9f8c1c909ffb241cc539402
Paulina Bujała <bujala.paulina@gmail.com>
2019-03-03 14:46:07 +0100

    Metody osoba 2

--- src/main/java/Magazyn.java
+++ src/main/java/Magazyn.java
@@ -2,12 +2,42 @@ import java.util.List;
 
 public class Magazyn {
     List <String> listaPracownikow;
-    List <String> listaProduktów;
+    List<Produkt> listaProduktów;
 
-    public Magazyn(List<String> listaPracownikow, List<String> listaProduktów) {
+    public Magazyn (List<String> listaPracownikow, List<Produkt> listaProduktów) {
         this.listaPracownikow = listaPracownikow;
         this.listaProduktów = listaProduktów;
     }
+
+    public void dodajProdukt (Produkt produkt) {
+        listaProduktów.add(produkt);
+    }
+
+    public void usunProdukt (String nazwa) {
+        for (Produkt p : listaProduktów) {
+            if (p.getNazwa().equals(nazwa)) {
+                listaProduktów.remove(p);
+                break;
+            }
+        }
+    }
+
+    public Produkt wypiszProdukt (String nazwa) {
+        Produkt produkt=null;
+        for (Produkt p: listaProduktów) {
+            if (p.getNazwa().equals(nazwa)){
+                produkt=p;
+            }
+        }
+        return produkt;
+    }
+
+    public List<Produkt> wypiszAllProdukty () {
+        return listaProduktów;
+    }
+
+
+
 
     @Override
     public String toString() {
--- src/main/java/Produkt.java
+++ src/main/java/Produkt.java
@@ -1,5 +1,37 @@
-public class Produkt implements  {
+public class Produkt {
+
     private String nazwa;
     private String cenaNetto;
-    private enum podatek;
+    private Podatek podatekVat;
+
+    public Produkt(String nazwa, String cenaNetto, Podatek podatekVat) {
+        this.nazwa = nazwa;
+        this.cenaNetto = cenaNetto;
+        this.podatekVat = podatekVat;
+    }
+
+    public Produkt(String nazwa) {
+        this.nazwa = nazwa;
+    }
+
+    public String getNazwa() {
+        return nazwa;
+    }
+
+    public String getCenaNetto() {
+        return cenaNetto;
+    }
+
+    public Podatek getPodatekVat() {
+        return podatekVat;
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt{" +
+                "nazwa='" + nazwa + '\'' +
+                ", cenaNetto='" + cenaNetto + '\'' +
+                ", podatekVat=" + podatekVat +
+                '}'+"\n";
+    }
 }
